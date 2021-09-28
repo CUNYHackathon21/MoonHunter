6ef464e978d281f99d775c8b616be73e557b78c5
Mazensh88 <63317015+Mazensh88@users.noreply.github.com>
2021-09-28 18:15:07 -0400

    backend is ready for work
    Yay!

--- js/home.js
+++ js/home.js
@@ -3,5 +3,8 @@ function work() {
 
   let d = x / 2;
 
-  document.getElementById("distance").innerHTML = d;
+  console.log(d);
+  document.getElementById("distance").value = d;
+
+  document.getElementById("time").innerHTML = "01:00:00";
 }
