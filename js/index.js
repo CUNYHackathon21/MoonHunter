a4349323e3ca1ac4bbb900f1d1cb7e99cd316eac
RoshHour <91507998+RoshHour@users.noreply.github.com>
2021-09-28 13:35:12 -0400

    Map now uses geo location to center at your location
    When visiting the page, you're now prompted to allow to use your location otherwise it falls back to the default center already hardcoded. Also fixed some grammatical issues.

--- js/index.js
+++ js/index.js
@@ -1,19 +1,29 @@
 
-//Google maps script.
-    let map;
+// Google maps script.
+let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
+
+  // Make the map center your current location.
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(setMapCenter);
+  }
 }
 
+function setMapCenter(position) {
+  let coords = position.coords;
+  map.setCenter(new google.maps.LatLng(coords.latitude,
+    coords.longitude));
+}
 
-//TypeIt script.
+// TypeIt script.
 
 new TypeIt(".simpleUsage", {
-    strings: "Welcome to Moon Hunter!, this web app will help photographers to plan their shot to take a perfect picture of the moon aliening with a building or any high object. All what you need to do is choose your points on the map then the app will calculate everything for you to take your shot.",
+    strings: "Welcome to Moon Hunter! This web app will help photographers to plan their shots to take a perfect picture of the moon aligning with a building or any other high object. All what you need to do is choose two points on the map then the app will calculate the time for you to take your shot.",
     speed: 50,
     waitUntilVisible: true,
-    }).go();
+  }).go();
