f01cc847e465d886ede0e0291b3bfb3a7a316ffd
Mazensh88 <63317015+Mazensh88@users.noreply.github.com>
2021-09-29 10:38:00 -0400

    PROJECT DONE
    -calculate the angle
    -distance taken from the map
    -moon set time working

--- js/home.js
+++ js/home.js
@@ -2,29 +2,33 @@ let markers = [];
 
 let distance = 0;
 
-
-function getTime(){
-
+function getTime() {
   let actualTime = new Date();
   let hour = actualTime.getHours();
   let minutes = actualTime.getMinutes();
   let seconds = actualTime.getSeconds();
-  let fullTime= hour+ " : " + minutes + " : " + seconds ;
+  let fullTime = hour + " : " + minutes + " : " + seconds;
 
-document.getElementById("time").innerHTML = fullTime;
+  document.getElementById("time").innerHTML = fullTime;
 }
 
-
 function work() {
-
   // Implement logic here.
-   getTime();
-   moonSetTime();
+  getTime();
+  //moonSetTime();
+  moonSetTime()
+    .then((resDa) => {
+      document.getElementById("moonTime").innerHTML =
+        "Moon set time is " + resDa.moonset;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
-  let x = parseInt(document.getElementById("building_height").value);
+  parseInt(document.getElementById("building_height").value);
 
+  result();
   document.getElementById("distance").value = distance;
-
 }
 
 function setup() {
@@ -38,11 +42,11 @@ function setup() {
 
 function createMarkers() {
   let markerA = new google.maps.Marker({
-      map: map,
-      draggable: true,
-      label: "Marker A",
-      animation: google.maps.Animation.DROP,
-      position: { lat: -34.397, lng: 150.644 },
+    map: map,
+    draggable: true,
+    label: "Marker A",
+    animation: google.maps.Animation.DROP,
+    position: { lat: -34.397, lng: 150.644 },
   });
 
   markers[markerA.label] = markerA;
@@ -54,17 +58,17 @@ function createMarkers() {
     animation: google.maps.Animation.DROP,
     position: { lat: -34, lng: 150 },
   });
-  
+
   markers[markerB.label] = markerB;
 
   // Trigger event handlers for on drag end (DragEnd)
-  google.maps.event.addListener(markerA, "dragend", function(event) {
+  google.maps.event.addListener(markerA, "dragend", function (event) {
     eventHandler.trigger("dragend", markerA, event);
   });
 
-  google.maps.event.addListener(markerB, "dragend", function(event) {
+  google.maps.event.addListener(markerB, "dragend", function (event) {
     eventHandler.trigger("dragend", markerB, event);
-  })
+  });
 
   setDistance();
 }
@@ -77,13 +81,12 @@ function onMarkerUpdate() {
   let center = map.getCenter();
 
   let lat = center.lat();
-  let lng = center.lng() - (elements.length * (distance / 2));
+  let lng = center.lng() - elements.length * (distance / 2);
 
   for (let i = 0; i < elements.length; i++) {
     let marker = elements[i];
 
-    marker.setPosition(
-      new google.maps.LatLng(lat, lng + (distance * i)));
+    marker.setPosition(new google.maps.LatLng(lat, lng + distance * i));
   }
 
   setDistance();
@@ -102,17 +105,17 @@ function setDistance() {
 
   distance = google.maps.geometry.spherical.computeDistanceBetween(
     elements[0].getPosition(),
-    elements[1].getPosition(),
-  )
+    elements[1].getPosition()
+  );
 
   console.log(distance);
 }
 
-
 function result() {
   let angle1 = 90.0;
-  let b = parseInt(document.getElementById("hight").value); //Adjacent
-  let c = parseInt(document.getElementById("distance").value); //Opposite
+  let b = parseInt(document.getElementById("building_height").value); //Adjacent
+  //let c = parseInt(document.getElementById("distance").value); //Opposite
+  let c = distance;
   //finding the Hypotenuse using Pythagorean theorem
 
   let a = Math.sqrt(Math.pow(b, 2) + Math.pow(c, 2));
@@ -132,38 +135,19 @@ function result() {
   let degree = missingAngle * (180 / Math.PI);
   console.log(degree.toFixed(2) + " degree"); //Convert a number into a string, rounding the number to keep only two decimals
 
-  let result = (document.getElementById("result").innerHTML =
+  let result = (document.getElementById("angle").value =
     degree.toFixed(2) + " Degree");
 }
 
-  function moonSetTime(){
+function moonSetTime() {
+  let API_KEY = "67458e9d671e48678bb2b16c74b5d85c";
+  const URL = "https://api.ipgeolocation.io/astronomy";
 
-      let API_KEY = "67458e9d671e48678bb2b16c74b5d85c";
-      const URL = "https://api.ipgeolocation.io/astronomy";
-    
-      const Full_URL = `${URL}?apiKey=${API_KEY}&lat=40.7128&long=-73.935242`;
-    
-      const moonPromise = fetch(Full_URL);
-    
-      return moonPromise.then((response) => {
-        return response.json();
-      });
-    };
-    
-    //console.log(getMoonData());
+  const Full_URL = `${URL}?apiKey=${API_KEY}&lat=40.7128&long=-73.935242`;
 
-    getMoonData()
-      .then((resDa) => {
-        console.log(resDa.moonset);
-        let time = resDa.moonset;
-        document.getElementById("moonTime").value= resDa.moonset;
-        let minInt = console.log(parseInt(time.slice(3, 5)) - 30);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  const moonPromise = fetch(Full_URL);
 
-    // document.getElementById("moonTime").value=6;
-   // document.getElementById("moonTime").value= resDa.moonset;
-   
-   }
+  return moonPromise.then((response) => {
+    return response.json();
+  });
+}
